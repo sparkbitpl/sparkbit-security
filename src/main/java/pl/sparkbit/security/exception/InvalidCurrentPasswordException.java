9f7ec8c878cab0cc5cf58824b760a96a010304d8
wojtekl <wojtekl@sparkbit.pl>
2018-02-22 16:32:46 +0100

    tm2-1338 Change password should fail with 403 when current password is invalid
    Change-Id: I65beb469182c14c3e320f9a2ce15473bdb4d352a

--- src/main/java/pl/sparkbit/security/exception/InvalidCurrentPasswordException.java
+++ src/main/java/pl/sparkbit/security/exception/InvalidCurrentPasswordException.java
@@ -2,9 +2,10 @@ package pl.sparkbit.security.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import pl.sparkbit.commons.exception.BusinessException;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InvalidCurrentPasswordException extends RuntimeException {
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class InvalidCurrentPasswordException extends BusinessException {
 
     public InvalidCurrentPasswordException(String message) {
         super(message);
