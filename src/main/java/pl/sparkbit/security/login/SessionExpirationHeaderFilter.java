0bd234936c4512b242925337fbe47a924b9d29f7
Michał Kręglewski <michal@sparkbit.pl>
2018-04-26 23:31:01 +0200

    scs-57 Expiration timestamp header is not returned when an error occurs
    Change-Id: Icf3e3eb85062c5d6e420dd5eb6d8d32e43717369

--- src/main/java/pl/sparkbit/security/login/SessionExpirationHeaderFilter.java
+++ src/main/java/pl/sparkbit/security/login/SessionExpirationHeaderFilter.java
@@ -1,48 +1,39 @@
 package pl.sparkbit.security.login;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 import pl.sparkbit.security.restauthn.AuthenticationTokenHelper;
 import pl.sparkbit.security.service.SessionService;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.Instant;
-import java.util.Optional;
 
 @RequiredArgsConstructor
-public class SessionExpirationHeaderFilter extends GenericFilterBean {
+public class SessionExpirationHeaderFilter extends OncePerRequestFilter {
 
     private final SessionService sessionService;
     private final String sessionExpirationTimestampHeaderName;
     private final AuthenticationTokenHelper authenticationTokenHelper;
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+    @SuppressWarnings("NullableProblems")
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-
-        chain.doFilter(req, res);
-
-        if (!sessionService.isSessionExpirationEnabled()) {
-            return;
+        if (sessionService.isSessionExpirationEnabled()) {
+            addSessionExpirationHeader(request, response);
         }
+        chain.doFilter(request, response);
+    }
 
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
-        Optional<String> authToken = authenticationTokenHelper.extractAuthenticationToken(request);
-
-        authToken.ifPresent((token) -> {
+    private void addSessionExpirationHeader(HttpServletRequest request, HttpServletResponse response) {
+        authenticationTokenHelper.extractAuthenticationToken(request).ifPresent((token) -> {
             Instant expirationTimestamp = sessionService.updateAndGetSessionExpirationTimestamp(token);
             response.setHeader(
-                    sessionExpirationTimestampHeaderName,
-                    String.valueOf(expirationTimestamp.toEpochMilli()));
+                    sessionExpirationTimestampHeaderName, String.valueOf(expirationTimestamp.toEpochMilli()));
         });
-
     }
 }
