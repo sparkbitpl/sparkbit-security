2cecf362a08ead0250d8966cf3fc661730583828
Michał Kręglewski <michal@sparkbit.pl>
2018-07-07 12:19:13 +0200

    scs-67 Test properties should be overridable by env variables
    Change-Id: I8310d87c212aae45f3417adf8ea34ff3624f659e

--- src/test/java/pl/sparkbit/security/dao/mybatis/MapperTestBase.java
+++ src/test/java/pl/sparkbit/security/dao/mybatis/MapperTestBase.java
@@ -1,8 +1,9 @@
 package pl.sparkbit.security.dao.mybatis;
 
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import pl.sparkbit.commons.test.AbstractMapperTests;
 
-@ContextConfiguration(classes = MapperTestConfig.class)
+@SpringBootTest(classes = MapperTestConfig.class)
 public abstract class MapperTestBase extends AbstractMapperTests {
 }
