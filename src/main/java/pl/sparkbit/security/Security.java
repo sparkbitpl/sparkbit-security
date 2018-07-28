42294a5a80118001efd982bc8c9d29ccb135048f
Michał Kręglewski <michal@sparkbit.pl>
2018-07-28 09:33:57 +0200

    scs-69 userId should be available from Security for login endpoint
    Change-Id: I898fcef87864a4d75fde37901a05da3bb590e8ee

--- src/main/java/pl/sparkbit/security/Security.java
+++ src/main/java/pl/sparkbit/security/Security.java
@@ -1,14 +1,17 @@
 package pl.sparkbit.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import pl.sparkbit.security.domain.RestUserDetails;
+import pl.sparkbit.security.login.LoginUserDetails;
 
 @Component
 @SuppressWarnings({"unused", "WeakerAccess"})
+@Slf4j
 public class Security {
 
     public static final String USER_ROLE_NAME = "ROLE_USER";
@@ -18,6 +21,7 @@ public class Security {
     public static final GrantedAuthority ADMIN_ROLE = new SimpleGrantedAuthority(ADMIN_ROLE_NAME);
     public static final GrantedAuthority SYSTEM_ROLE = new SimpleGrantedAuthority(SYSTEM_ROLE_NAME);
 
+    // This method is useful for endpoints other than login
     public RestUserDetails currentUserDetails() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null && isUserAuthenticated()) {
@@ -27,8 +31,26 @@ public class Security {
     }
 
     public String currentUserId() {
-        RestUserDetails restUserDetails = currentUserDetails();
-        return restUserDetails != null ? restUserDetails.getUserId() : null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+
+        Object principal = auth.getPrincipal();
+        if (principal instanceof RestUserDetails) {
+            if (auth.isAuthenticated() && isEntityInRole(auth, USER_ROLE)) {
+                return ((RestUserDetails) principal).getUserId();
+            }
+            log.warn("Unauthenticated user with RestUserDetails principal - should never happen");
+        } else if (principal instanceof LoginUserDetails) {
+            if (auth.isAuthenticated()) {
+                return ((LoginUserDetails) principal).getUserId();
+            }
+            log.warn("Unauthenticated user with LoginUserDetails principal - should never happen");
+        } else {
+            log.warn("Unexpected type of authenticated principal");
+        }
+        return null;
     }
 
     public boolean isUserAuthenticated() {
@@ -44,10 +66,14 @@ public class Security {
     }
 
     /**
-     * Can be used for users and systems. Authenticated users will always have ROLE_USER, system will have ROLE_SYSTEM
+     * Can be used for users and systems. Authenticated users will always have ROLE_USER, systems will have ROLE_SYSTEM
      */
     public boolean isAuthenticatedEntityInRole(GrantedAuthority role) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return isEntityInRole(auth, role);
+    }
+
+    public boolean isEntityInRole(Authentication auth, GrantedAuthority role) {
         return auth != null && auth.getAuthorities().contains(role);
     }
 }
