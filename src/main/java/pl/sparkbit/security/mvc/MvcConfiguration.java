d2bd429e7486cf3f48cd3ee858540566778da897
Michał Kręglewski <michal@sparkbit.pl>
2018-07-31 17:20:55 +0200

    SPKT-224 API documentation not available
    Change-Id: Id6d3dd7ab478a3185fd7b2572e43c2790d524e31

--- src/main/java/pl/sparkbit/security/mvc/MvcConfiguration.java
+++ src/main/java/pl/sparkbit/security/mvc/MvcConfiguration.java
@@ -1,18 +1,18 @@
 package pl.sparkbit.security.mvc;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import pl.sparkbit.security.config.SecurityProperties;
 
 @Configuration
 @RequiredArgsConstructor
-public class MvcConfiguration extends WebMvcConfigurationSupport {
+public class MvcConfiguration {
 
     private final SecurityProperties configuration;
 
-    @Override
+    @Bean
     protected RequestMappingHandlerMapping createRequestMappingHandlerMapping() {
         return new EndpointMappings(configuration.getPaths());
     }
