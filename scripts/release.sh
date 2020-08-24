eb63989d57283885e0ec7e694d405a6541208629
Michał Kręglewski <michal@sparkbit.pl>
2020-08-24 13:29:37 +0200

    Switch to releasing to Maven central repo
    Change-Id: I52e061c84e509ec2fdb5b69cd2a3036d3fcbbaad

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,35 +4,20 @@ set -x
 
 . `dirname $0`/setVars.sh
 
-if [ -z ${dryRun+x} ] || [ "$dryRun" = "" ]; then
-    export dryRun=false
-fi
 export projectName=sparkbit-security
 
 cd $repoRoot
 
 scripts/bump_version.sh
-if [ $dryRun = "false" ]; then
-    mvn install deploy
-else
-    echo dryRun mode - Skipping deploying to nexus
-    mvn install
-fi
+mvn -s "${NEXUS_SETTINGS_XML}" -P release -DskipTests -Dcheckstyle.skip -Dmaven.antrun.skip clean deploy
+
 
 git commit -a -m "RELEASE: release $projectName-$RELEASE_VERSION"
-if [ $dryRun = "false" ]; then
-    git tag -am "$projectName-$RELEASE_VERSION" $projectName-$RELEASE_VERSION
-    git push origin master
-else
-    echo dryRun mode - Skipping tagging version and pushing changes to remote
-fi
+git tag -am "$projectName-$RELEASE_VERSION" $projectName-$RELEASE_VERSION
+git push origin master
 
 unset currentVersion
 
 scripts/bump_version.sh
 git commit -a -m "RELEASE: prepare for next development iteration"
-if [ $dryRun = "false" ]; then
-    git push origin master
-else
-     echo dryRun mode - Skipping pushing changes to remote
-fi
+git push origin master
